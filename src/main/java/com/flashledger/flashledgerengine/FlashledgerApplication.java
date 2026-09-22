32406b7a0a62ecdddebc977bfcfce7f68863bc24
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-09-22 11:40:11 +0530

    initial commit

---
+++ src/main/java/com/flashledger/flashledgerengine/FlashledgerApplication.java
@@ -0,0 +1,13 @@
+package com.flashledger.flashledgerengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashledgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashledgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flashledger/flashledgerengine/FlashledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashledger.flashledgerengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashledgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
